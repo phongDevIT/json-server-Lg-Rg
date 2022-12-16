660ce9b32b66cbb4c8581641ed2cdcd7b4108edd
ViTrans <tran.vi487@gmail.com>
2022-12-17 00:34:08 +0700

    upload source code

--- main.js
+++ main.js
@@ -1,34 +1,25 @@
-function signup(e) {
-  event.preventDefault();
-  var username = document.getElementById("username").value;
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
-  var user = {
-    username: username,
-    email: email,
-    password: password,
-  };
-  var json = JSON.stringify(user);
-  localStorage.setItem(username, json);
-  alert("dang ky thanh cong");
-}
-function login(e) {
-  event.preventDefault();
-  var username = document.getElementById("username").value;
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
-  var user = localStorage.getItem(username);
-  var data = JSON.parse(user);
-  if (!user) {
-    alert("vui long nhap username password");
-  } else if (
-    username == data.username &&
-    email == data.email &&
-    password == data.password
-  ) {
-    alert("dang nhap thanh cong");
-    window.location.href = "todolist.html";
+let apiUser = "http://localhost:3000/user";
+const username = document.querySelector(".input-signup-username");
+const password = document.querySelector(".input-signup-password");
+const bntSignup = document.querySelector(".signup__signInButton");
+
+bntSignup.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (username.value == "" || password.value == "") {
+    alert("Please enter your username and password");
   } else {
-    alert("dang nhap that bai");
+    const user = {
+      username: username.value,
+      password: password.value,
+    };
+    fetch(apiUser, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
   }
-}
+});
